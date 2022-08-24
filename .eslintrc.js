612b6937d08a656da93e32fab774be795f220063
Mateus de Oliveira <mateusdeoliveira1530@gmail.com>
2022-08-24 20:04:26 -0300

    chore: alterando eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': 2,
+    'no-console': 'error',
     'no-var': 'error',
   },
 };
